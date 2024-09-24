70cf3918c0a9480f256c2b88cb9676c52a4b4f94
iridi-x <tyler.jay.c@gmail.com>
2024-09-24 14:51:09 -0700

    Finished section 1, manual weight calculation and model comparison

--- least_square_method.py
+++ least_square_method.py
@@ -9,9 +9,34 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import PolynomialFeatures, StandardScaler, Normalizer
 from sklearn.linear_model import LinearRegression
 
+
 # store resuslts of each model
 results = []
 
+
+# ======================================
+def min_max_scaling(x):
+    return (x - np.min(x)) / (np.max(x) - np.min(x))
+
+
+def scale_0_1(x):
+    return x / np.max(x)
+
+
+
+def scale_minus1_1(x):
+    return 2 * (x - np.min(x)) / (np.max(x) - np.min(x)) - 1
+
+
+def z_score_normalization(x):
+    return (x - np.mean(x)) / np.std(x)
+
+
+def mean_normalization(x):
+    return (x - np.mean(x)) / (np.max(x) - np.min(x))
+
+
+# ======================================
 
 # scale data
 def scale_data(data):
@@ -53,80 +78,65 @@ def import_wine_data_from_csv():
     return wine_X, wine_y
 
 
-
-# EXAMPLE FROM LECTURE SLIDES 
-def regressionExampleDiabetes():
-    print("least square solution")
-
-    # load the dataset
-    diabetes_X, diabetes_y = datasets.load_diabetes(return_X_y=True)
-
-    # use only one feature
-    diabetes_X = diabetes_X[:, np.newaxis, 2]
-
-    # split the data into train and test    
-    diabetes_X_train = diabetes_X[:-20]
-    diabetes_X_test  = diabetes_X[-20:]
-
-    # split targets into train and test
-    diabetes_y_train = diabetes_y[:-20] 
-    diabetes_y_test  = diabetes_y[-20:] 
-
-    # create linear regression obj
-    lregr = linear_model.LinearRegression()
-
-    # train the model
-    lregr.fit(diabetes_X_train, diabetes_y_train)
-
-    # make predictions using the testing set 
-    diabetes_y_predic = lregr.predict(diabetes_X_test)
-    print("coeffcients \n", lregr.coef_)
-
-    # print the MSE (mean standard error)
-    print("Mean squared error: %.2f" % mse(diabetes_y_test, diabetes_y_predic))
-
-    # the coef of determination: (1 is a perfect score)
-    print("Coefficient of determination: %.2f" % r2_score(diabetes_y_test, diabetes_y_predic))
-
-    # plot outputs 
-    plt.scatter(diabetes_X_test, diabetes_y_test, color="black")
-    plt.plot(diabetes_X_test, diabetes_y_predic, color="blue", linewidth=3)
-    plt.xticks(())
-    plt.yticks(())
-    plt.show()
+def calculate_w(data_X, data_y):
+    print("Manual Weights:")
+    X = np.array(data_X)
+    y = np.array(data_y)
+    X_t = X.T
+    X_t_X = np.dot(X_t, X)
+    X_t_y = np.dot(X_t, y)
+    X_t_X_inv = np.linalg.inv(X_t_X)
+    w_hat =  np.dot(X_t_X_inv, X_t_y)
+    # print("Weights: \n", w_hat)
+    return w_hat
 
 
-def least_square_solution():
-    print("lssq")
+def least_square_solution(train_data_X, train_data_y, X_new):
+    print("Training target statistics:")
+    print(f"Min: {np.min(train_data_y)}, Max: {np.max(train_data_y)}, Mean: {np.mean(train_data_y)}")
+
+
+    X = np.array(train_data_X)
+    y = np.array(train_data_y)
+    X_t = X.T
+    X_t_X = np.dot(X_t, X)
+    X_t_y = np.dot(X_t, y)
+    X_t_X_inv = np.linalg.inv(X_t_X)
+
+    # w_hat represents the weight vector
+    w_hat =  np.dot(X_t_X_inv, X_t_y)
+    X_new = np.array(X_new)
+    w_hat_t = w_hat.T
+
+    # this one seems to produce similar results, not sure why as it does not follow the formula in the notes
+    # y_hat = np.dot(X_new, w_hat)
+
+    y_hat = []
+    for row in X_new:
+        y_i = np.dot(w_hat_t, row)
+        y_hat.append(y_i)
+
+    y_hat = np.array(y_hat)
+
+    return y_hat
+
+def print_results(results):
+        # print results of all models
+        print(f"{'Type':<15} {'Degree/Order':<15} {'Training RMSE':<15} {'Training R²':<15} {'Training Time':<15} {'Testing RMSE':<15} {'Testing R²':<15}")
+        for result in results:
+            print(f"{result['Type']:<15} {result['Order']:<15.0f} {result['Training RMSE']:<15.6f} {result['Training R2']:<15.6f} {result['Training Time']:<15.6f} {result['Testing RMSE']:<15.6f} {result['Testing R2']:<15.6f}")
 
 
 
-
-def least_square_method_np():
-
-    # load the dataset
-    # wine_X, wine_y = datasets.load_wine(return_X_y=True)  
-    wine_X, wine_y = import_wine_data_from_csv()
-
-    #  normalize the data 
-    # wine_y = normalize_data(wine_y)
-    wine_X = normalize_data(wine_X)
-
-    # wine_X = scale_data(wine_X)
-    # wine_y = scale_data(wine_y)
-
-    # 80 - 20 split, training to testing
-    wine_X_train, wine_X_test, wine_y_train, wine_y_test = train_test_split(wine_X,wine_y, test_size=0.2, random_state=42)
+def least_square_method_np(wine_X, wine_y):
 
     for order in range(1, 6): # this tests orders 1-5 
+
+        wine_X_train, wine_X_test, wine_y_train, wine_y_test = train_test_split(wine_X,wine_y, test_size=0.2, random_state=(42+order))
 
         order = PolynomialFeatures(order) # this uses Sklearn to create polynomial features from dataset
         X_train_polynomial = order.fit_transform(wine_X_train)
         X_test_polynomial = order.transform(wine_X_test)
-
-        # alternate attempt
-        # X_train_polynomial = order.transform(wine_X_train)
-        # X_test_polynomial = order.transform(wine_X_test)
 
         # train and benchmark the model
         model = LinearRegression()
@@ -155,20 +165,9 @@ def least_square_method_np():
         intercept = model.intercept_
 
 
-        if order.degree == 2:
-            print("Weights (coefficients):", weights)
-            print("Intercept (bias):", intercept)
-            
-            print("Polynomial function \n")
-            terms = []
-            for i, coef in enumerate(weights):
-                if i == 0:
-                    terms.append(f"{intercept:.2f}")  # The intercept term
-                else:
-                    terms.append(f"{coef:.2f} * x^{i}")
-
         # add data object to results array 
         results.append({
+            "Type": "Sklearn",
             "Order": order.degree,
             # RMSE measures the distance from prediction to actual, with 0 being no different between predic and actual
             "Training RMSE": training_rmse,
@@ -178,26 +177,65 @@ def least_square_method_np():
             "Testing RMSE": test_rmse,
             "Testing R2":  test_r2
         })
-        
- 
-    # print results of all models
-    print(f"{'Degree/Order':<15} {'Training RMSE':<15} {'Training R²':<15} {'Training Time':<15} {'Testing RMSE':<15} {'Testing R²':<15}")
-    for result in results:
-        print(f"{result['Order']:<15.0f} {result['Training RMSE']:<15.6f} {result['Training R2']:<15.6f} {result['Training Time']:<15.6f} {result['Testing RMSE']:<15.6f} {result['Testing R2']:<15.6f}")
 
-   
-   
-    # create model - we need to split the data and pass it now 
-    # lsm_np = np.linalg.lstsq()
+        # for our selected model
+        if order.degree == 2:
+            print("order:", order.degree)
+            print("Intercept: \n", intercept,"\n Weights: \n", weights)
+
+
+            print("\n Calculate w manually and compare: \n")
+            manual_w = calculate_w(X_train_polynomial, wine_y_train)
+            print(manual_w.T)
+
+            # treain manual model
+            man_start_time = time.time()
+
+            train_prediction = least_square_solution(X_train_polynomial, wine_y_train,  X_train_polynomial)
+            test_prediction = least_square_solution(X_train_polynomial, wine_y_train,  X_test_polynomial)
+
+            man_end_time = time.time()
+
+            # stats
+            man_training_rmse = np.sqrt(mse(wine_y_train, train_prediction))
+            man_train_r2      = r2_score(wine_y_train, train_prediction) 
+
+            man_test_rmse     = np.sqrt(mse(wine_y_test, test_prediction))
+            man_test_r2       = r2_score(wine_y_test, test_prediction)
+
+            man_training_time = (man_end_time - man_start_time)
+
+            # append stats
+            results.append({
+                "Type": "manual",
+                "Order": order.degree,
+                # RMSE measures the distance from prediction to actual, with 0 being no different between predic and actual
+                "Training RMSE": man_training_rmse,
+                # R2 is somewhat inverse to RMSE, where a value of 1 indicates no difference between prediction and actual
+                "Training R2": man_train_r2,
+                "Training Time": man_training_time,
+                "Testing RMSE": man_test_rmse,
+                "Testing R2":  man_test_r2
+            })
+                
+    return results
+
 
 
 
 def main():
     print("main")
-    # regressionExampleDiabetes()
-    least_square_method_np()
+    # import data 
+    wine_X, wine_y = import_wine_data_from_csv()
+
+    # preprocess data
+    print(f"Preprocessed: \n {wine_X[:1]}")
+    wine_X = scale_0_1(wine_X)
+    print(f"Processed: \n {wine_X[:1]} \n\n")
 
 
+    results = least_square_method_np(wine_X, wine_y)    
+    print_results(results)
 
 
 if __name__ == '__main__':
